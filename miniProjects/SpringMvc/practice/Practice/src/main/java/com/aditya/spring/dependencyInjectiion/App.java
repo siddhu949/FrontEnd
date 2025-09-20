82dece1785d11db4618017593ec2420a251b69f3
siddhu949 <adityasatyaprakash9@gmail.com>
2025-09-20 12:51:42 +0530

    setter dependency injection using xml property tag

---
+++ miniProjects/SpringMvc/practice/Practice/src/main/java/com/aditya/spring/dependencyInjectiion/App.java
@@ -0,0 +1,17 @@
+package com.aditya.spring.dependencyInjectiion;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.aditya.spring.dependencyInjectiion.pojo.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ap= new ClassPathXmlApplicationContext("com/aditya/spring/dependencyInjection/xml/spring.xml");
+		Bean b= (Bean)ap.getBean("bean");
+		b.hello("aditya");
+		
+
+}
+}
