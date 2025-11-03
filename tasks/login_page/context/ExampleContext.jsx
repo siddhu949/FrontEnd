4350d8194c7e4e3862dacfe74af28a9b7fdb95f2
siddhu949 <adityasatyaprakash9@gmail.com>
2025-11-03 21:47:30 +0530

    adding swiggy-clone app

--- tasks/login_page/context/ExampleContext.jsx
+++ tasks/login_page/context/ExampleContext.jsx
@@ -1,14 +1,16 @@
 import { createContext, useState ,useContext} from "react";
+// craete context
 const GiftContext = createContext();
 
-
+//provider function
 export const GiftProvider =({children})=>{
     const [surprise,setSurprise]=useState("apple");//game=cricket,captain=dhoni-- destruct is surprise.game and surprise.captain
-
+//custom component
     return(
         <GiftContext.Provider value={{surprise,setSurprise}}>
         {children}
         </GiftContext.Provider>
     )
 }
+//custom hook
 export const useData =()=>useContext(GiftContext)
